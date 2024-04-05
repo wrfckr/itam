c9bd4835546ba4ed6583cfd3bf42fa8f8fd194ed
avtetkin <wrfckr@gmail.com>
2024-04-05 17:06:20 +0500

    new ccs

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
 
 app.use(expressLayouts)
 app.set('layout', './layouts/default')
